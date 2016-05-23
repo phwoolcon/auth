36129031dc87d79a96f645473c8ab93b94077196
Christopher CHEN <chenshaoming@appgame.com>
2016-05-23 19:09:01 +0800

    docs: update sample config file

--- config/auth.php
+++ config/auth.php
@@ -1,20 +1,34 @@
 <?php
 
 return [
-    'adapter' => 'Generic',
+    'adapter' => 'Phwoolcon\Auth\Adapter\Generic',
     'options' => [
-        'user_model' => 'Phwoolcon\User',
-        'login_form' => '',
-        'register_form' => '',
-    ],
-    'routes' => [
-        'GET' => [
-            'account/login' => function () {},
-            'account/logout' => function () {},
+        'user_model' => 'Phwoolcon\Model\User',
+        'user_fields' => [
+            'login_fields' => ['username', 'email', 'mobile'],
+            'password_field' => 'password',
         ],
-        'POST' => [
-            'account/register' => function () {},
-            'account/login' => function () {},
+        'session_key' => 'front',
+        'remember_login' => [
+            'cookie_key' => 'remember',
+            'ttl' => 604800,
         ],
+        'uid_key' => 'id',
+        'security' => [
+            'default_hash' => Phalcon\Security::CRYPT_BLOWFISH_Y,
+            'work_factor' => 5,
+        ],
+        'hints' => [
+            'invalid_password' => 'Invalid password',
+            'invalid_user_credential' => 'Invalid user credential',
+            'user_credential_registered' => 'User credential registered',
+            'unable_to_save_user' => 'Unable to save user',
+        ],
+        'register' => [
+            'confirm_mobile' => false,
+            'confirm_email' => false,
+            'confirmation_code_ttl' => 604800,
+        ],
+        'redirect_timeout' => 2,
     ],
 ];
