56a88cca6b2609c1bf700c0f268541e2db1b7737
Christopher CHEN <chenshaoming@appgame.com>
2016-06-16 21:35:55 +0800

    feat(sso): encrypt sso data

--- src/Controller/SsoTrait.php
+++ src/Controller/SsoTrait.php
@@ -5,8 +5,11 @@ namespace Phwoolcon\Auth\Controller;
 use Exception;
 use Phwoolcon\Auth\Auth;
 use Phwoolcon\Auth\Model\SsoSite;
+use Phwoolcon\Crypt;
 use Phwoolcon\DateTime;
 use Phwoolcon\Log;
+use Phwoolcon\Model\User;
+use Phwoolcon\Security;
 
 trait SsoTrait
 {
@@ -16,10 +19,20 @@ trait SsoTrait
         return $initToken == md5(md5(fnGet($site, 'id') . $initTime) . fnGet($site, 'site_secret'));
     }
 
+    protected function encryptSsoData($ssoData)
+    {
+        $site = $ssoData['site'];
+        unset($ssoData['user'], $ssoData['site']);
+        $key = fnGet($site, 'site_secret');
+        $ssoData['user_data'] = Crypt::opensslEncrypt(json_encode($ssoData['user_data']), $key);
+        $ssoData['sign'] = Security::signArrayHmacSha256($ssoData, $key);
+        return $ssoData;
+    }
+
     protected function getSsoUserData($input)
     {
         try {
-            $site = SsoSite::getSiteByReturnUrl(fnGet($input, 'notifyUrl'));
+            $site = SsoSite::getSiteDataByReturnUrl(fnGet($input, 'notifyUrl'));
             $initToken = fnGet($input, 'initToken');
             $initTime = fnGet($input, 'initTime');
             if (!$this->checkInitToken($initTime, $initToken, $site)) {
@@ -37,6 +50,7 @@ trait SsoTrait
                     'avatar' => $user->getAvatar(),
                 ],
                 'user' => $user,
+                'site' => $site,
             ];
             return $ssoData;
         } catch (Exception $e) {
